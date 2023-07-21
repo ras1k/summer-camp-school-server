403d2050e5b63bb1a0e87c834a4efeddb7390f6f
Rasik Abdullah <rasikabdullah@gmail.com>
2023-07-22 01:44:22 +0600

    admin api added

--- index.js
+++ index.js
@@ -64,6 +64,46 @@ async function run() {
             res.send(result)
         });
 
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            console.log(user);
+            const query = { email: user.email };
+            const existingUser = await usersCollection.findOne(query);
+            console.log("existing user", existingUser);
+            if (existingUser) {
+                return res.send({ message: 'user already exist' })
+            }
+            const result = await usersCollection.insertOne(user);
+            res.send(result)
+        });
+
+        app.get('/users/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+
+            if (req.decoded.email !== email) {
+                res.send({ admin: false })
+            }
+
+            const query = { email: email }
+            const user = await usersCollection.findOne(query);
+            const result = { admin: user?.role === 'admin' };
+            res.send(result);
+        });
+
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const updateDoc = {
+              $set: {
+                role: 'admin'
+              },
+            };
+      
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.send(result)
+          })
+
+
         //carts
         app.post('/carts', async (req, res) => {
             const courseItem = req.body;
